67fdf2b79057e314cc17ced11e88dd7fe54c3736
Adam Green <greena88@gmail.com>
2011-11-13 15:24:43 +0000

    Added wallpaper scrolling option.

--- src/com/android/launcher2/Preferences.java
+++ src/com/android/launcher2/Preferences.java
@@ -42,6 +42,7 @@ public class Preferences {
 
     static final String PREF_HOMESCREEN_ENDLESS_LOOP = "EndlessHomescreenLoop";
     static final String PREF_LAUNCHER_ORIENTATE = "LauncherOrientation";
+    static final String PREF_WALLPAPER_SCROLLING = "WallpaperScrolling";
 
     public boolean getEndlessScrolling() {
         return mPreferences.getBoolean(PREF_HOMESCREEN_ENDLESS_LOOP, true);
@@ -49,5 +50,9 @@ public class Preferences {
     
     public boolean getOrientate() {
         return mPreferences.getBoolean(PREF_LAUNCHER_ORIENTATE, true);
+    }
+
+    public boolean getWallpaperScrolling() {
+        return mPreferences.getBoolean(PREF_WALLPAPER_SCROLLING, true);
     }
 }
